ea332f754fa6445b80b4b29c5caed3257082c750
Abdalaziz <alsaydi23@yahoo.com>
2019-07-21 18:50:34 -0400

    fixed the getQuadrilateralKovRule test

--- src/main/test/java/sarf/kov/KovRulesManagerTest.java
+++ src/main/test/java/sarf/kov/KovRulesManagerTest.java
@@ -47,36 +47,24 @@ class KovRulesManagerTest {
 
     private static Stream<Arguments> getQuadrilateralVerbs() {
         return Stream.of(
-                Arguments.of("صبر", 1),
-                Arguments.of("مدد", 2),
-                Arguments.of("ءجج", 3),
-                Arguments.of("ءنء", 4),
-                Arguments.of("ءكل", 5),
-                Arguments.of("سءر", 6),
-                Arguments.of("مرء", 7),
-                Arguments.of("ودد", 8), // مثال واوي مضعف
-                Arguments.of("وءد", 9),
-                Arguments.of("وكء", 10),
-                Arguments.of("وكر", 11),
-                Arguments.of("يسس", 12),
-                Arguments.of("يءس", 13),
-                Arguments.of("يسر", 14),
-                Arguments.of("ءوب", 15),
-                Arguments.of("لوء", 16),
-                Arguments.of("سود", 17),
-                Arguments.of("ءيس", 18),
-                Arguments.of("شيء", 19),
-                Arguments.of("بيع", 20),
-                Arguments.of("ءسو", 21),
-                Arguments.of("مءو", 22),
-                Arguments.of("غزو", 23),
-                Arguments.of("ءبي", 24),
-                Arguments.of("رءي", 25),
-                Arguments.of("رمي", 26),
-                Arguments.of("ءوي", 27),
-                Arguments.of("شوي", 28),
-                Arguments.of("وءي", 29),
-                Arguments.of("وقي", 30)
+                Arguments.of("دحرج", 1),
+                Arguments.of("زلزل", 2),
+                Arguments.of("دءدء", 3),
+                Arguments.of("طمءن", 4),
+                Arguments.of("غرقء", 4),
+                Arguments.of("بءدل", 4),
+                Arguments.of("يءيء", 5),
+                Arguments.of("وءوء", 5),
+                Arguments.of("وسوس", 6),
+                Arguments.of("يعيع", 6),
+                Arguments.of("يرنء", 7),
+                Arguments.of("يرنء", 7),
+                Arguments.of("رهيء", 8),
+                Arguments.of("تودء", 8),
+                Arguments.of("شريف", 9),
+                Arguments.of("قلسي", 10),
+                Arguments.of("ضوضي", 11),
+                Arguments.of("حيحي", 11)
         );
     }
 
@@ -107,7 +95,13 @@ class KovRulesManagerTest {
         assertEquals(kindOfVerb, actual.getKov());
     }
 
-    @Test
-    void getQuadrilateralKovRule() {
+    @ParameterizedTest
+    @MethodSource("getQuadrilateralVerbs")
+    void getQuadrilateralKovRule(String verb, int kindOfVerb) {
+        var sut = KovRulesManager.getInstance();
+        var actual = sut.getQuadrilateralKovRule(verb.charAt(0), verb.charAt(1), verb.charAt(2), verb.charAt(3));
+
+        assertNotNull(actual);
+        assertEquals(kindOfVerb, actual.getKov());
     }
 }
