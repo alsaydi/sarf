0a609d07591fc02a3b988129d165d50b11db1935
Abdalaziz <alsaydi23@yahoo.com>
2019-09-29 16:20:16 -0400

    Update RunSarfCucumberTest.java

--- src/test/java/sarftests/RunSarfCucumberTest.java
+++ src/test/java/sarftests/RunSarfCucumberTest.java
@@ -7,5 +7,5 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 //@CucumberOptions(plugin = {"html:output"})
 @CucumberOptions(plugin = {"pretty"}, strict = true)
-public class RunCucumberTest {
+public class RunSarfCucumberTest {
 }
