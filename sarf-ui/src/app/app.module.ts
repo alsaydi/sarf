7854e8902e85baac4a07ee13f410040dd4351782
Abdalaziz <alsaydi23@yahoo.com>
2019-10-09 22:40:00 -0400

    fixing up the UI

--- sarf-ui/src/app/app.module.ts
+++ sarf-ui/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './material-module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ sarf-ui/src/app/material-module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule} from '@angular/material';
+
+@NgModule({
+    exports: [
+        MatToolbarModule
+    ]
+})
+export class AppMaterialModule { }
