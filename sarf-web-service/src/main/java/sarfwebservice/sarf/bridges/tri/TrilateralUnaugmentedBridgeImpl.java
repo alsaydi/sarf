06a2c83c2d722de8fd5dc7ba7bdc7bd011772f37
Abdalaziz Alsaydi <>
2021-10-08 22:58:45 -0400

    Removed unused imports

--- sarf-web-service/src/main/java/sarfwebservice/sarf/bridges/tri/TrilateralUnaugmentedBridgeImpl.java
+++ sarf-web-service/src/main/java/sarfwebservice/sarf/bridges/tri/TrilateralUnaugmentedBridgeImpl.java
@@ -1,4 +1,6 @@
 package sarfwebservice.sarf.bridges.tri;
+import java.util.List;
+
 /*
  *
  * MIT License
@@ -25,31 +27,13 @@ package sarfwebservice.sarf.bridges.tri;
  */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sarf.*;
-import sarf.gerund.modifier.trilateral.unaugmented.meem.TrilateralUnaugmentedMeemModifier;
-import sarf.gerund.modifier.trilateral.unaugmented.nomen.TrilateralUnaugmentedNomenModifier;
+
+import sarf.ConjugationResult;
+import sarf.KindOfVerb;
+import sarf.SystemConstants;
+import sarf.Word;
+import sarf.WordPresenter;
 import sarf.gerund.modifier.trilateral.unaugmented.quality.TrilateralUnaugmentedQualityModifier;
-import sarf.gerund.modifier.trilateral.unaugmented.standard.UnaugmentedTrilateralStandardGerundModifier;
-import sarf.gerund.trilateral.unaugmented.QualityGerundConjugator;
-import sarf.gerund.trilateral.unaugmented.TrilateralUnaugmentedGerundConjugator;
-import sarf.gerund.trilateral.unaugmented.TrilateralUnaugmentedNomenGerundConjugator;
-import sarf.gerund.trilateral.unaugmented.meem.MeemGerundConjugator;
-import sarf.noun.TrilateralUnaugmentedNouns;
-import sarf.noun.trilateral.unaugmented.UnaugmentedTrilateralActiveParticipleConjugator;
-import sarf.noun.trilateral.unaugmented.UnaugmentedTrilateralPassiveParticipleConjugator;
-import sarf.noun.trilateral.unaugmented.assimilate.AssimilateAdjectiveConjugator;
-import sarf.noun.trilateral.unaugmented.elative.ElativeNounConjugator;
-import sarf.noun.trilateral.unaugmented.exaggeration.NonStandardExaggerationConjugator;
-import sarf.noun.trilateral.unaugmented.exaggeration.StandardExaggerationConjugator;
-import sarf.noun.trilateral.unaugmented.instrumental.StandardInstrumentalConjugator;
-import sarf.noun.trilateral.unaugmented.modifier.activeparticiple.ActiveParticipleModifier;
-import sarf.noun.trilateral.unaugmented.modifier.assimilate.AssimilateModifier;
-import sarf.noun.trilateral.unaugmented.modifier.elative.ElativeModifier;
-import sarf.noun.trilateral.unaugmented.modifier.exaggeration.ExaggerationModifier;
-import sarf.noun.trilateral.unaugmented.modifier.instrumental.InstrumentalModifier;
-import sarf.noun.trilateral.unaugmented.modifier.passiveparticiple.PassiveParticipleModifier;
-import sarf.noun.trilateral.unaugmented.modifier.timeandplace.TimeAndPlaceModifier;
-import sarf.noun.trilateral.unaugmented.timeandplace.TimeAndPlaceConjugator;
 import sarf.verb.trilateral.unaugmented.UnaugmentedImperativeConjugator;
 import sarf.verb.trilateral.unaugmented.UnaugmentedTrilateralRoot;
 import sarf.verb.trilateral.unaugmented.active.ActivePastConjugator;
@@ -57,8 +41,6 @@ import sarf.verb.trilateral.unaugmented.active.ActivePresentConjugator;
 import sarf.verb.trilateral.unaugmented.modifier.UnaugmentedTrilateralModifier;
 import sarf.verb.trilateral.unaugmented.passive.PassivePastConjugator;
 import sarf.verb.trilateral.unaugmented.passive.PassivePresentConjugator;
-
-import java.util.List;
 
 @Service
 public class TrilateralUnaugmentedBridgeImpl implements TrilateralUnaugmentedBridge {
